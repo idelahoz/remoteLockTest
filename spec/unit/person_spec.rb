56a2fe3210b1ee874436cdd11a7c3db34d620029
Israel De La Hoz <israeldelahoz@gmail.com>
2021-08-29 14:16:51 -0500

    feat(implement-parsing): add city mapping

--- spec/unit/person_spec.rb
+++ spec/unit/person_spec.rb
@@ -15,12 +15,24 @@ RSpec.describe Person do
     Person.new(person_data)
   }
 
-
-
   describe "#to_s" do
     it "should return the correct string representation" do
       expect(person.to_s).to eq("marcus, paris, 2/18/1991")
     end
-  end
 
+    describe "with a city in the mapping list" do
+      let(:person_data){ 
+        {
+          "first_name" => "marcus", 
+          "city" => "NYC", 
+          "birthdate" => "18-2-1991", 
+          "last_name" => "carlsen"
+        }
+      }
+
+      it "should return the right city in the #to_s method" do
+        expect(person.to_s).to eq("marcus, New York City, 2/18/1991")
+      end
+    end
+  end
 end
